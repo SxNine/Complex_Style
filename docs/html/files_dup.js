a11cb2f84c371cdaf02c13aea269327b3076c9e8
Nikita Bekish <sxnine@internet.ru>
2023-12-03 17:01:00 +0500

    Changes to be committed:  1.Added commentaries to default files from oxygen  2.Added docs. from doxygen in html.

---
+++ docs/html/files_dup.js
@@ -0,0 +1,5 @@
+var files_dup =
+[
+    [ "mycomplex.cpp", "mycomplex_8cpp.html", "mycomplex_8cpp" ],
+    [ "mycomplex.h", "mycomplex_8h.html", "mycomplex_8h" ]
+];
---
+++ docs/html/search/searchdata.js
@@ -0,0 +1,27 @@
+var indexSectionsWithContent =
+{
+  0: "acmos~",
+  1: "c",
+  2: "m",
+  3: "acos~",
+  4: "o"
+};
+
+var indexSectionNames =
+{
+  0: "all",
+  1: "classes",
+  2: "files",
+  3: "functions",
+  4: "related"
+};
+
+var indexSectionLabels =
+{
+  0: "Указатель",
+  1: "Классы",
+  2: "Файлы",
+  3: "Функции",
+  4: "Друзья"
+};
+
